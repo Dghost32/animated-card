287eda21c9ead2b06b886f599e37e5d72092c516
Carlos Jimenez <cajimenezca@unal.edu.co>
2021-08-26 20:54:18 -0500

    cleaner code

--- app.js
+++ app.js
@@ -1,35 +1,24 @@
 let cards = document.querySelectorAll(".card");
 
-cards.forEach((card) => {
-  let content = card.children.item(1);
-  let btn = card.children.item(2);
-  card.addEventListener("click", () => handleCardClick(card, content, btn));
-});
+/* VanillaTilt.init(cards, {
+  max: 20,
+  speed: 300,
+  gyroscope: true,
+  transition: true,
+}); */
 
-function handleCardClick(card, content, btn) {
-  console.log(content.offsetHeight);
-  if (!card.classList.contains("show")) {
-    card.style.height = "60vh";
-    setTimeout(function () {
-      if (card.classList.contains("show")) {
-        content.style.display = "block";
-        content.style.height = "auto";
-        btn.classList.remove("down");
-        btn.classList.add("up");
-      }
-    }, 300);
-  } else {
-    card.style.height = "15vh";
-    content.style.display = "none";
-    content.style.height = "0";
-    btn.classList.remove("up");
-    btn.classList.add("down");
-  }
+cards.forEach((card) =>
+  card.addEventListener("click", () => handleCardClick(card))
+);
+
+function handleCardClick(card) {
+  !card.classList.contains("show")
+    ? (card.style.height = "60vh")
+    : (card.style.height = "15vh");
+
   toggleShow(card);
 }
 
 function toggleShow(card) {
-  let classes = card.classList;
-  if (classes.contains("show")) classes.remove("show");
-  else classes.add("show");
+  card.classList.toggle("show");
 }
